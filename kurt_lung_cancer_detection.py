499f1f4a1752224f1af436c4f9993ab179258763
Kurt Ruegg <kruegg@Kurts-MacBook-Pro.local>
2017-01-22 14:33:48 -0800

    eda

--- kurt_lung_cancer_detection.py
+++ kurt_lung_cancer_detection.py
@@ -1,5 +1,6 @@
-import numpy as np # linear algebra
-import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+import cv2
+import numpy as np
+import pandas as pd
 import dicom
 import os
 import pylab
@@ -8,6 +9,52 @@ import time
 import matplotlib.pyplot as plt
 
 DATA_PATH = '/Volumes/My Passport for Mac/stage1/'
+
+"""
+Dataset:
+1596 patients
+
+Each patient is composed of a number of slices. Each slice has the following
+attributes:
+'AcquisitionNumber'
+'BitsAllocated'
+'BitsStored'
+'Columns'
+'FrameOfReferenceUID'
+'HighBit'
+'ImageOrientationPatient'
+'ImagePositionPatient'
+'InstanceNumber'
+'KVP'
+'Laterality'
+'LongitudinalTemporalInformationModified'
+'Modality'
+'PatientBirthDate'
+'PatientID'
+'PatientName'
+'PhotometricInterpretation'
+'PixelData'
+'PixelRepresentation'
+'PixelSpacing'
+'PositionReferenceIndicator'
+'PregnancyStatus'
+'RescaleIntercept'
+'RescaleSlope'
+'Rows'
+'SOPClassUID'
+'SOPInstanceUID'
+'SamplesPerPixel'
+'SeriesDescription'
+'SeriesInstanceUID'
+'SeriesNumber'
+'SliceLocation'
+'SliceThickness' -- distance between slices in millimeters
+'SpecificCharacterSet'
+'StudyInstanceUID'
+'WindowCenter'
+'WindowWidth'
+'pixel_array'
+"""
 
 # Timing function
 def timeit(method):
@@ -23,15 +70,33 @@ def timeit(method):
         return result
     return timed
 
-def show_slice(s):
+def show_slice(s, hist_equalize = False):
     """
     Input:
         s -- dicom slice object
     Output:
-        plots 2-D image of slice
+        Plots 2-D image of slice. The top image is the standard image, the
+        bottom is histogram equalized (increases contrast)
     """
+    pylab.subplot(2, 1, 1)
     pylab.imshow(s.pixel_array, cmap=pylab.cm.bone)
+
+    if hist_equalize:
+        pylab.subplot(2, 1, 2)
+        pylab.imshow(cv2.equalizeHist(s.pixel_array.astype(np.uint8)),
+                     cmap=pylab.cm.bone)
     pylab.show()
+
+@timeit
+def stack_slices(slices):
+    """
+    Input:
+        slices -- list of slice objects
+    Output:
+        3-dimensional numpy array
+    """
+    image_3d = np.stack([s.pixel_array for s in slices])
+    return image_3d
 
 @timeit
 # Load the scans in given folder path
@@ -39,19 +104,36 @@ def load_scan(path):
     slices = [dicom.read_file(path + '/' + s) for s in os.listdir(path)]
     slices.sort(key = lambda x: int(x.InstanceNumber))
     try:
-        slice_thickness = np.abs(slices[0].ImagePositionPatient[2] - slices[1].ImagePositionPatient[2])
+        slice_thickness = np.abs(slices[0].ImagePositionPatient[2] - \
+                                 slices[1].ImagePositionPatient[2])
     except:
         slice_thickness = np.abs(slices[0].SliceLocation - slices[1].SliceLocation)
 
     for s in slices:
         s.SliceThickness = slice_thickness
-
-    print len(slices)
-    for s in slices:
-        print s.SliceThickness
         print s.PixelSpacing
-        print '\n'
+
+    print slices[0]
+    for i in dir(slices[0]):
+        print "\'{}\'".format(i)
+    return slices
 
 if __name__ == "__main__":
-    # load_scan(DATA_PATH + 'ff8599dd7c1139be3bad5a0351ab749a')
-    load_scan(DATA_PATH + '0a0c32c9e08cc2ea76a71649de56be6d')
+    # patient = '0a0c32c9e08cc2ea76a71649de56be6d'
+    # patient = 'ff8599dd7c1139be3bad5a0351ab749a'
+    # stack_slices(load_scan(DATA_PATH + patient))
+
+    number_of_slices = []
+    slice_thicknesses = []
+    pixel_spacing = []
+
+    ctr = 0
+    number_patients = len(os.listdir(DATA_PATH))
+    for patient in os.listdir(DATA_PATH):
+        if patient[0] != '.':
+            slices = load_scan(DATA_PATH + patient)
+            slice_thicknesses.append(slices[0].SliceThickness)
+            number_of_slices.append(len(slices))
+            pixel_spacing.append(slices[0].PixelSpacing)
+        print slice_thicknesses
+        print float(ctr) / number_patients
